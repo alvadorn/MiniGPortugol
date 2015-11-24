023a931f4ef9f9c5c23eb361d08e2fa29ce94b89
Igor Sant'ana <igorba2005@gmail.com>
2015-11-23 22:09:31 -0200

    Fixing comments, division, subtraction analysis

--- src/lexanalyzer/states.h
+++ src/lexanalyzer/states.h
@@ -5,7 +5,7 @@
 #include <vector>
 #include <cstdint>
 
-typedef enum symbol { NUMBER = 0, DOT, APOSTROPHE, D_SLASH,
+typedef enum symbol { NUMBER = 0, DOT, APOSTROPHE, SLASH,
   ANYTHING_EXCPT_B_LINE, BREAK_LINE,  LETTER, UNDERSCORE, EQUAL, LESS_THAN,
   GREATER_THAN, MATH_OP, COLON, COMMA, OPEN_PARENT, CLOSE_PARENT,
   SEMICOLON, UNIDENTIFIED, MINUS } Symbol;
@@ -15,7 +15,7 @@ namespace MiniGPortugol {
   private:
     std::map<uint8_t, std::map<Symbol, uint8_t>> states;
     std::vector<uint8_t> recognize_states = { 1, 3, 6, 8, 9, 10, 11, 12, 13,
-      14, 15, 16, 17, 18, 19, 20 };
+      14, 15, 16, 17, 18, 19, 20, 21 };
     void createMap();
   public:
     States();
